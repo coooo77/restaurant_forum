c33ac0040a4570919c3a86c992448c86128aad5c
TL Chang <coooo77@hotmail.com>
2020-04-26 21:21:46 +0800

    Initialize sequelize and connect to database

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const handlebars = require('express-handlebars') // 引入 handlebars
+const db = require('./models') // 引入資料庫
 const app = express()
 const port = 3000
-app.engine('handlebars', handlebars()) // Handlebars 註冊樣板引擎
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+// Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+  db.sequelize.sync()
+  console.log(`Example app listening on port ${port}`)
 })
+
+// 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
+require('./routes')(app)
