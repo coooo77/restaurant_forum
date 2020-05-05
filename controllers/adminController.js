5a59af85c9d06c3413c38d35fbc58bbb1d04c774
TL Chang <coooo77@hotmail.com>
2020-05-05 18:01:31 +0800

    Add categories selector on admin create and edit page, Add CategoryId to admin postRestaurant and putRestaurant and Add selected option condition to category select

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -18,7 +18,14 @@ const adminController = {
   },
 
   createRestaurant: (req, res) => {
-    return res.render('admin/create')
+    Category.findAll({
+      raw: true,
+      nest: true
+    }).then(categories => {
+      return res.render('admin/create', {
+        categories: categories
+      })
+    })
   },
 
   postRestaurant: (req, res) => {
@@ -38,6 +45,7 @@ const adminController = {
           opening_hours: req.body.opening_hours,
           description: req.body.description,
           image: file ? img.data.link : null,
+          CategoryId: req.body.categoryId
         }).then((restaurant) => {
           req.flash('success_messages', 'restaurant was successfully created')
           return res.redirect('/admin/restaurants')
@@ -51,7 +59,8 @@ const adminController = {
         address: req.body.address,
         opening_hours: req.body.opening_hours,
         description: req.body.description,
-        image: null
+        image: null,
+        CategoryId: req.body.categoryId
       }).then((restaurant) => {
         req.flash('success_messages', 'restaurant was successfully created')
         return res.redirect('/admin/restaurants')
@@ -59,11 +68,6 @@ const adminController = {
     }
   },
 
-  // getRestaurant: (req, res, callback) => {
-  //   return Restaurant.findByPk(req.params.id, { include: [Category] }).then(restaurant => {
-  //     callback({ restaurant: restaurant.toJSON() })
-  //   })
-  // },
   getRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id, {
       raw: true,
@@ -77,8 +81,16 @@ const adminController = {
   },
 
   editRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, { raw: true }).then(restaurant => {
-      return res.render('admin/create', { restaurant: restaurant })
+    Category.findAll({
+      raw: true,
+      nest: true
+    }).then(categories => {
+      return Restaurant.findByPk(req.params.id).then(restaurant => {
+        return res.render('admin/create', {
+          categories: categories,
+          restaurant: restaurant.toJSON()
+        })
+      })
     })
   },
 
@@ -101,6 +113,7 @@ const adminController = {
               opening_hours: req.body.opening_hours,
               description: req.body.description,
               image: file ? img.data.link : restaurant.image,
+              CategoryId: req.body.categoryId
             })
               .then((restaurant) => {
                 req.flash('success_messages', 'restaurant was successfully to update')
@@ -118,7 +131,8 @@ const adminController = {
             address: req.body.address,
             opening_hours: req.body.opening_hours,
             description: req.body.description,
-            image: restaurant.image
+            image: restaurant.image,
+            CategoryId: req.body.categoryId
           })
             .then((restaurant) => {
               req.flash('success_messages', 'restaurant was successfully to update')
