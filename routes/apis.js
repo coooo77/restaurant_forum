0042350ea32b32e41f8ea63f5ddc8a9ab5a9c776
TL Chang <coooo77@hotmail.com>
2020-05-12 15:53:23 +0800

    GET /api/admin/restaurants/:id

--- routes/apis.js
+++ routes/apis.js
@@ -5,6 +5,7 @@ const adminController = require('../controllers/api/adminController.js')
 const categoryController = require('../controllers/api/categoryController')
 
 router.get('/admin/restaurants', adminController.getRestaurants)
+router.get('/admin/restaurants/:id', adminController.getRestaurant)
 
 router.get('/admin/categories', categoryController.getCategories)
 
