14bc808bba798d5c4d3581f5882da59a3a051b3d
TL Chang <coooo77@hotmail.com>
2020-05-13 22:55:06 +0800

    add authenticated and authenticatedAdmin

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const handlebars = require('express-handlebars') // 引入 handlebars
 const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 const session = require('express-session')
-const passport = require('./config/passport')
+
 const methodOverride = require('method-override')
 const db = require('./models') // 引入資料庫
 const app = express()
@@ -11,6 +11,8 @@ const port = process.env.PORT || 3000
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+const passport = require('./config/passport')
+
 app.engine('handlebars', handlebars({
   defaultLayout: 'main',
   helpers: require('./config/handlebars-helpers')
--- routes/apis.js
+++ routes/apis.js
@@ -2,12 +2,22 @@ const express = require('express')
 const router = express.Router()
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
+const passport = require('../config/passport')
+const authenticated = passport.authenticate('jwt', { session: false })
+const authenticatedAdmin = (req, res, next) => {
+  if (req.user) {
+    if (req.user.isAdmin) { return next() }
+    return res.json({ status: 'error', message: 'permission denied' })
+  } else {
+    return res.json({ status: 'error', message: 'permission denied' })
+  }
+}
 
 const adminController = require('../controllers/api/adminController.js')
 const categoryController = require('../controllers/api/categoryController')
 const userController = require('../controllers/api/userController.js')
 
-router.get('/admin/restaurants', adminController.getRestaurants)
+router.get('/admin/restaurants', authenticated, authenticatedAdmin, adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
 router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
 router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
